77d1da81cd151346e4b17f3758f83a63dfb3a6da
jhLim97 <wnsgur2749@naver.com>
2021-11-17 01:28:01 +0900

    feat: offscreencanvas api를 이용한 이미지 로드 및 렌더링
    * 이전까지의 실습에서는 offscreencanvas api를 활용하기 보다는 web
      worker에서 이미지 로드를 통해 성능향상을 위주로 진행했습니다.
    * 이번 실습에서는 offscreencanvas api를 통해 워커 쓰레드에서 캔버스
      렌더링까지 처리하도록 구현했습니다.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,11 +1,8 @@
 import React, { useRef, useEffect } from "react";
 
-const objCanvas = document.createElement('canvas');
 const PlayerCanvas = () => {
   const canvasRef = useRef(null);
   const objectList = ['/buildBuilding.png', '/buildObject.png', '/chat.png', '/fileUpload.png', '/logout.png', '/person.png', '/setting.png', '/users.png', '/voiceChat.png'];
-  let x = 50;
-  let y = 50;
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -13,30 +10,20 @@ const PlayerCanvas = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const ctx = canvas.getContext('2d');
-
-    const offscreen = objCanvas.transferControlToOffscreen();
+    const offscreen = canvas.transferControlToOffscreen();
     const worker = new Worker("../../workers/offscreencanvas.js", {
       type: "module",
     });
 
     worker.onmessage = async (e) => {
-      const { imageBitmapList } = e.data;
-    
-      offscreen.width = window.innerWidth;
-      offscreen.height = window.innerHeight;
-      const offscreenCtx = offscreen.getContext('2d');
-      imageBitmapList.forEach(imageBitmap => {
-        offscreenCtx.drawImage(imageBitmap, x, y, 100, 100);
-        x += 110;    
-      });
-  
-      ctx.drawImage(offscreen, 0, 0);
+      const { msg, success } = e.data;
+      if (success) alert(msg);
+
       worker.terminate();
     };
 
     const init = () => {
-      worker.postMessage({ objectList: objectList });
+      worker.postMessage({ offscreen, objectList }, [offscreen]);
     };
     init();
   }, []);
--- src/workers/offscreencanvas.js
+++ src/workers/offscreencanvas.js
@@ -3,8 +3,19 @@ import makeImageBitMapList from "../utils/drawCharacter";
 const worker = self;
 
 worker.onmessage = async (e) => {
-  const { objectList } = e.data;
+  const { offscreen, objectList } = e.data;
 
   const imageBitmapList = await makeImageBitMapList(objectList);
-  worker.postMessage({ imageBitmapList });
+
+  const offscreenCtx = offscreen.getContext('2d');
+  let x = 50;
+  let y = 50;
+
+  console.log(imageBitmapList);
+  imageBitmapList.forEach(imageBitmap => {
+    offscreenCtx.drawImage(imageBitmap, x, y, 100, 100);
+    x += 110;    
+  });
+
+  worker.postMessage({ msg: 'Drawing images on offscreencanvas is finish', success: true });
 };
