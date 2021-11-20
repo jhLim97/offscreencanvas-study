5d5e3360128bd9512e08959ec998090a83ebd658
jhLim97 <wnsgur2749@naver.com>
2021-11-21 02:38:37 +0900

    offscreencanvas api를 이용한 백그라운드캔버스 렌더링 in worker thread
    * offscreencanvas api를 이용해 worker thread에서 백그라운드캔버스를 렌더링 했습니다.
    * worker thread에서는 작업 후 transferToImageBitmap를 이용해 그려진
      이미지를 반환합니다.
    * 메인 thread에서는 반환된 이미지를 presentation 캔버스에
      덮어쓰기합니다.
    * 이로 인한 장점은 worker thread로의 작업 분담 및 백그라운드 캔버스의
      이미지를 복사하는 것이 아닌 버퍼의 포인터를 참조함으로써, 부담을 훨씬
      줄일 수 있습니다.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,25 +1,32 @@
 import React, { useRef, useEffect } from "react";
 
+const offscreen = new OffscreenCanvas(window.innerWidth, window.innerHeight);
+let ctx;
 const PlayerCanvas = () => {
   const canvasRef = useRef(null);
   const objectList = ['/buildBuilding.png', '/buildObject.png', '/chat.png', '/fileUpload.png', '/logout.png', '/person.png', '/setting.png', '/users.png', '/voiceChat.png'];
 
   useEffect(() => {
     const canvas = canvasRef.current;
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    ctx = canvas.getContext('2d');
 
-    const offscreen = canvas.transferControlToOffscreen();
     const worker = new Worker("../../workers/offscreencanvas.js", {
       type: "module",
     });
 
     worker.onmessage = async (e) => {
-      const { msg, success } = e.data;
-      if (success) alert(msg);
+      const { msg, backImage } = e.data;
 
-      worker.terminate();
+      // 버퍼에 대한 포인터만 가져옴 => 카피가 안 일어나서 더 효율적
+      // 결론 backgroundCanvas의 전체 이미지 카피없이 포인터만으로 굉장히 효율적인 렌더링을 수행할 수 있음
+      ctx.drawImage(backImage, 0, 0, window.innerWidth, window.innerHeight);
+
+      if (msg === 'stop') return;
+
+      worker.postMessage({ objectList }, []);
+      //worker.terminate();
     };
 
     const init = () => {
--- src/workers/offscreencanvas.js
+++ src/workers/offscreencanvas.js
@@ -1,21 +1,38 @@
 /* eslint-disable no-restricted-globals */
 import makeImageBitMapList from "../utils/drawCharacter";
 const worker = self;
+let offscreenCanvas;
+let offscreenCtx;
 
 worker.onmessage = async (e) => {
   const { offscreen, objectList } = e.data;
 
+  if (offscreen === undefined) {
+    offscreenCtx.rect(100, 300, 100, 100);
+    offscreenCtx.fill();
+
+    const backImage = offscreenCanvas.transferToImageBitmap();
+    console.log(backImage);
+    worker.postMessage({ msg: 'stop', backImage });
+    return;
+  }
+
+  offscreenCanvas = offscreen;
   const imageBitmapList = await makeImageBitMapList(objectList);
 
-  const offscreenCtx = offscreen.getContext('2d');
+  offscreenCtx = offscreenCanvas.getContext('2d');
   let x = 50;
   let y = 50;
 
-  console.log(imageBitmapList);
   imageBitmapList.forEach(imageBitmap => {
     offscreenCtx.drawImage(imageBitmap, x, y, 100, 100);
     x += 110;    
   });
 
-  worker.postMessage({ msg: 'Drawing images on offscreencanvas is finish', success: true });
+  offscreenCtx.rect(100, 500, 100, 100);
+  offscreenCtx.fill();
+
+  const backImage = offscreenCanvas.transferToImageBitmap();
+  console.log(backImage);
+  worker.postMessage({ msg: 'Drawing images on offscreencanvas is finish', backImage });
 };
